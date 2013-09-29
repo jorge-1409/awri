fe3850a9e7f530657ba23a6f4a7b7b59cabaff21
Jorge Villada <jorge-1409@hotmail.com>
2013-09-29 11:27:12 -0500

    se actualiza el referer

--- public/practica_3/insertar.php
+++ public/practica_3/insertar.php
@@ -32,7 +32,7 @@
             </ul>
         </fieldset>
         <p class="right">
-            <a href="http://validator.w3.org/check?uri=http://awri.my.phpcloud.com/practica_3/index.xhtml">
+            <a href="http://validator.w3.org/check?uri=referer">
                 <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
             </a>
             <a href="http://jigsaw.w3.org/css-validator/check/referer">
